fc7f98f37192dfff2f3501894ec8717c5a406648
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-08-23 16:43:53 -0300

    ex7 feito

---
+++ semana4/exercicios/exercicio7.c
@@ -0,0 +1,77 @@
+// Faça um programa que programa que gere números inteiros e aleatórios para uma matriz
+//20x20. Faça uma versão monothread, uma com 2 thread e outra com 4 thread. Qual possui o menor
+//tempo de execução?
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+
+#define MONOTHREAD 1
+#define TWO_THREADS 2
+#define FOUR_THREADS 4
+
+int main(){
+    int i, j, k;
+    int **matriz;
+    int num_threads;
+    clock_t start, end;
+    double cpu_time_used;
+    pthread_t threads[FOUR_THREADS];
+    int rc;
+    long t;
+    printf("Digite o número de threads: ");
+    scanf("%d", &num_threads);
+    printf("Digite o tamanho da matriz: ");
+    scanf("%d", &k);
+    matriz = (int **)malloc(k*sizeof(int *));
+
+    for(i=0; i<k; i++){
+        matriz[i] = (int *)malloc(k*sizeof(int));
+    }
+
+    for(i=0; i<k; i++){
+        for(j=0; j<k; j++){
+            matriz[i][j] = rand()%100;
+        }
+    }
+
+    if(num_threads == MONOTHREAD){
+        start = clock();
+        for(i=0; i<k; i++){
+            for(j=0; j<k; j++){
+                printf("%d ", matriz[i][j]);
+            }
+            printf("\n");
+        }
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("Tempo de execução: %f\n", cpu_time_used);
+    }
+
+    if(num_threads == TWO_THREADS){
+        start = clock();
+        for(i=0; i<k; i++){
+            for(j=0; j<k; j++){
+                printf("%d ", matriz[i][j]);
+            }
+            printf("\n");
+        }
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("Tempo de execução: %f\n", cpu_time_used);
+    }
+    
+    if(num_threads == FOUR_THREADS){
+        start = clock();
+        for(i=0; i<k; i++){
+            for(j=0; j<k; j++){
+                printf("%d ", matriz[i][j]);
+            }
+            printf("\n");
+        }
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        printf("Tempo de execução: %f\n", cpu_time_used);
+    }
+}
