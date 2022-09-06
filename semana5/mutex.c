4561401fa38103a39592da407514a7c99336086d
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-09-06 08:35:15 -0300

    first commit

--- semana5/mutex.c
+++ semana5/mutex.c
@@ -1,73 +1,70 @@
-// Faça um programa que calcule a soma dos elementos de um vetor.
-
-// Seu programa deverá receber dois parâmetros: o tamanho de um vetor (>=100) e um número de threads (>=2).
-
-// O resultado do programa é o somatório dos elementos deste vetor.
-
-// O programa deve criar um vetor do tamanho especificado, iniciá-lo com valores sintéticos (randomicos ou fixos, por exemplo, 1, 2, 3, ...) e reparti-lo entre as threads criadas, conforme o número de threads informado. Por exemplo, se o vetor tiver 100 posições, com 5 threads, então, cada thread irá somar 20 números do vetor.
-// Orientação a ser seguida: o somatório deve ser realizado em uma variável compartilhada, sendo utilizado um mutex para coordenar o acesso a ela.
-
-// mandar o vetor, e o que ela deve somar (de qual ponto ela deve iniciar até qual ponto somar)
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <malloc.h>
 #include <pthread.h>
+typedef struct { 
+	int begin, //posição inicial do vetor 
+	    end,   //posição final do vetor
+	    *v;    //vetor
+} Input;
 
-struct arguments {
-    int *vetor;
-    int pontoIncial;
-    int pontoFinal;
-};
+int acum=0;
+pthread_mutex_t m=PTHREAD_MUTEX_INITIALIZER;
 
-struct arguments args;
-
-int tamanhoVetor = 0;
-int numThreads = 0;
-int vetorDividido = 0;
-
-void* thread_func(void *arg){
-    struct arguments* args = (struct arguments *) arg;
-    int* result = malloc(sizeof(int));
-
-    *result = 0;
-
-    for (int i= args -> pontoIncial; i <= args -> pontoFinal ; i++){
-        *result += args->vetor[i];
-    }
-
-    return result;
+void* soma( void* par ) {
+  int i, somaL;
+  Input *in = (Input*) par;
+  somaL = 0;
+  for( i = in->begin ; i < in->end ; i++ )
+    somaL += in->v[i];
+  printf("S = %d\n",somaL);
+  pthread_mutex_lock(&m);
+  acum += somaL;
+  pthread_mutex_unlock(&m);
+  return NULL;
 }
 
-int main(int argc, char *argv[]){
-    tamanhoVetor = atoi(argv[1]);
-    numThreads = atoi(argv[2]);
 
-    vetorDividido = tamanhoVetor / numThreads;
+int main( int argc, char **argv ) {
+  int i, tam, nthreads, *vet;
+  pthread_t *tid;
+  Input *dta;
 
-    if (tamanhoVetor < 100 || numThreads < 2) {
-        printf("O tamanho do vetor deve ser maior que 100 e o número de threads deve ser maior que 2!");
+  if( argc != 3 ) {
+    fprintf(stderr,"Uso: acumula <tamanhoVetor> <numeroThreads>\n");
+    exit(1);
+  }
+  if( (tam = atoi(argv[1])) < 100 ) {
+    fprintf(stderr,"O tamanho do vetor deve ser maior ou igual à 100.\n");
+    exit(1);
+  }
+  if( (nthreads = atoi(argv[2])) < 2 ) {
+    fprintf(stderr,"O numero de threads deve ser maior ou igual à 2.\n");
+    exit(1);
+  }
 
-        return;
-    }
+  tid = (pthread_t*) malloc( nthreads*sizeof(pthread_t) );
+  dta = (Input*) malloc( nthreads*sizeof(Input) );
+  vet = (int*) malloc( tam*sizeof(int) );
 
-    pthread_t threads[numThreads];
+  for( i = 0 ; i < tam ; ++i ) 
+    vet[i] = 1;
 
-    for (int i = 0; i < numThreads; i++) {
-        pthread_create(&(threads[i]), NULL, thread_func, NULL);
-    }
-
-    for (int i = 0; i < numThreads; i++){
-        pthread_join(threads[i], NULL);
-    }
-
-    // for(int i= 0; i < tamanhoVetor; i++){
-    //     printf("entrou");
-    //     args.vetor[i] = 2;
-    //     printf("[%d]: %d", i, args.vetor[i]);
-    // }
-
-    free(args.vetor);
-
-    return 0;
+  for( i = 0 ; i < nthreads ; ++i ) {
+    dta[i].v = vet;
+    dta[i].begin = i*(tam/nthreads);
+    if ((i+1==nthreads)&&(tam%nthreads!=0))
+         dta[i].end = (i+1)*(tam/nthreads)+tam%nthreads;
+    else
+      dta[i].end = (i+1)*(tam/nthreads);
+    pthread_create( &(tid[i]), NULL, soma, (void*)&(dta[i]) );
+  }
+  for( i = 0 ; i < nthreads ; ++i )
+    pthread_join( tid[i], (void**)NULL );
+ 
+  printf("Resultado da soma = %d\n", acum);
+  free(tid);
+  free(dta);
+  free(vet);
+  return 0;
 }
