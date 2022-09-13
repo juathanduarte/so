f9fc4e8b9c23c0fefe7689cef7afd7939b0a21df
juathanduarte <juathan.cduarte@inf.ufpel.edu.br>
2022-09-13 16:30:24 -0300

    banqueiros start

---
+++ semana7 - TALVEZ QUESTÃO PROVA/banqueiros.c
@@ -0,0 +1,71 @@
+// Implemente o algoritmo do Banqueiro
+// – Pergunte ao usuário a quantidade de Processos e Recursos.
+// – Gere aleatoriamente a quantidade total de cada recurso. Ex.: 0 a 9.
+// – Gere aleatoriamente a quantidade necessária de cada recurso por
+// processo. Ex.: de 0 a 100% de cada recurso.
+// – Gere aleatoriamente a quantidade de recurso alocado inicialmente para
+// cada processo. Ex.: 0 a 50%.
+// – Espera-se como resultado, algo como:
+// • Imprimir todos os dados gerados aleatoriamente e a sequência segura
+// (ou uma informação que não é possível uma sequência segura)
+// • Ex.: A sequência segura é: Processo1 -> Processo3 -> Processo4 ->
+// Processo0 -> Processo2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int processos = 0;
+    int recursos = 0;
+
+    printf("Digite a quantidade de processos: ");
+    scanf("%d", &processos);
+
+    printf("Digite a quantidade de recursos: ");
+    scanf("%d", &recursos);
+
+    printf("\n");
+
+    int recursosAlocados[processos][recursos];
+
+    for (int i=0; i < processos; i++){
+        for (int j=0; j < recursos; j++){
+            recursosAlocados[i][j] = rand() % 10;
+            printf("%d ", recursosAlocados[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    // int recursosAlocados[recursos];
+    int recursosDisponiveis[recursos];
+    int recursosExistentes[recursos];
+
+    for (int i=0; i < recursos; i++){
+        recursosExistentes[i] = rand() % 10; // Gere aleatoriamente a quantidade total de cada recurso. Ex.: 0 a 9.
+        recursosDisponiveis[i] = recursosExistentes[i];
+        printf("%d ", recursosExistentes[i]);
+    }
+
+    printf("\n\n");
+
+    int recursosNecessarios[processos][recursos];
+
+    for (int i=0; i < processos; i++){
+        for (int j=0; j < recursos; j++){
+            recursosNecessarios[i][j] = rand() % (recursosExistentes[j] + 1);
+            printf("%d ", recursosNecessarios[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i=0; i < recursos; i++){
+        printf("%d ", recursosDisponiveis[i]);
+    }
+
+    printf("\n");
+
+    int recursosAlocadosTemp[processos][recursos];
+    
+}
