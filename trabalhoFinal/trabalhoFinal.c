4eddfb7a37b9c2bbd66930330aa1c024b0f34b60
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-11-11 19:28:15 -0300

    implementação step

--- trabalhoFinal/trabalhoFinal.c
+++ trabalhoFinal/trabalhoFinal.c
@@ -1,6 +1,6 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
 
 typedef struct Financiamento {
@@ -10,6 +10,7 @@ typedef struct Financiamento {
   float taxas;
   float subsidio;
   int financiado;
+  int momentoFinanciamento;
 } financiamento;
 
 typedef struct Banco {
@@ -27,7 +28,7 @@ int contadorSequenciaSegura = 0;
 pthread_mutex_t mutexBanco = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutexSequenciaSegura = PTHREAD_MUTEX_INITIALIZER;
 
-void imprimeSequenciaSegura(int boolean);
+void imprimeSequenciaSegura(int boolean, financiamento *pessoas);
 void pedirFinanciamento(financiamento *pessoa);
 void situacaoBanco(int boolean);
 void devolverDinheiro(financiamento *pessoa);
@@ -35,6 +36,7 @@ void emprestimoBanco(financiamento *pessoa);
 void verificaPessoasAtendidas();
 
 int qtdPessoas;
+int countWhile = 0;
 
 int main() {
   system("clear || cls");
@@ -51,7 +53,7 @@ int main() {
 
   } while (qtdPessoas < 1);
 
-  sequenciaSegura = (int*) malloc(qtdPessoas * sizeof(int));
+  sequenciaSegura = (int *)malloc(qtdPessoas * sizeof(int));
 
   financiamento pessoas[qtdPessoas];
 
@@ -73,7 +75,7 @@ int main() {
     pessoas[i].taxas = (float)(rand() % (int)pessoas[i].totalImovel);
 
     int subsidio = rand() % 2;
-    
+
     switch (subsidio) {
     case 0:
       pessoas[i].subsidio = (float)((int)pessoas[i].totalImovel * 0.05);
@@ -91,10 +93,10 @@ int main() {
     printf("\nPessoa %d -----\n->Total do imovel: R$ %.2f\n->Especie: R$ %.2f\n->Taxas: R$ %.2f\n->Subsidio: R$ %.2f\n", pessoas[i].id, pessoas[i].totalImovel, pessoas[i].especie, pessoas[i].taxas, pessoas[i].subsidio);
   }
 
-  int countWhile = 0;
   int boolean = 0;
 
   while ((contadorSequenciaSegura < qtdPessoas) && (countWhile < qtdPessoas)) {
+    printf("\n--------------- INSTANTE %d ---------------\n", countWhile);
     for (int i = 0; i < qtdPessoas; i++) {
       int erro = pthread_create(&financiamentos[i], NULL, (void *)pedirFinanciamento, (void *)&pessoas[i]);
       if (erro) {
@@ -112,13 +114,18 @@ int main() {
 
   if (contadorSequenciaSegura < qtdPessoas) {
     printf("\n\n\t----- ESTADO INSEGURO -----\n");
-    imprimeSequenciaSegura(boolean);
-    printf("->Pessoas que nao foram atendidas: %d\n", qtdPessoas - pessoasAtendidas);
+    imprimeSequenciaSegura(boolean, pessoas);
+    printf("->Pessoas que nao foram atendidas: \n");
+    for (int i = 0; i < qtdPessoas; i++) {
+      if (pessoas[i].financiado == 0) {
+        printf("->Pessoa %d", pessoas[i].id);
+      }
+    }
     printf("\n");
   } else {
     boolean = 1;
     printf("\n\n\t----- ESTADO SEGURO -----\n");
-    imprimeSequenciaSegura(boolean);
+    imprimeSequenciaSegura(boolean, pessoas);
     printf("\n");
   }
 
@@ -128,7 +135,7 @@ int main() {
   free(sequenciaSegura);
 }
 
-void imprimeSequenciaSegura(int boolean) {
+void imprimeSequenciaSegura(int boolean, financiamento *pessoas) {
   if (boolean == 1) {
     printf("\nSequencia segura:\n");
   } else {
@@ -136,23 +143,23 @@ void imprimeSequenciaSegura(int boolean) {
   }
 
   for (int i = 0; i < contadorSequenciaSegura; i++) {
-      printf("Pessoa %d", sequenciaSegura[i]);
-      if (i != contadorSequenciaSegura - 1) {
-        printf("-->");
-      }
+    printf("Pessoa %d no momento %d", sequenciaSegura[i], pessoas[sequenciaSegura[i]].momentoFinanciamento);
+    if (i != contadorSequenciaSegura - 1) {
+      printf("-->");
     }
-    printf("\n");
+  }
+  printf("\n");
 }
 
 void pedirFinanciamento(financiamento *pessoa) {
-  if (pessoa -> financiado == 0){
+  if (pessoa->financiado == 0) {
     int boolean;
 
-    printf("\nPessoa %d pedindo financiamento\n", pessoa -> id);
-    
+    printf("\nPessoa %d pedindo financiamento\n", pessoa->id);
+
     emprestimoBanco(pessoa);
 
-    if (pessoa -> financiado == 1){
+    if (pessoa->financiado == 1) {
       boolean = 1;
       situacaoBanco(boolean);
       printf("\nPessoa %d esta pagando as parcelas...\n", pessoa->id);
@@ -172,7 +179,7 @@ void pedirFinanciamento(financiamento *pessoa) {
 }
 
 void situacaoBanco(int boolean) {
-  if (boolean == 1){
+  if (boolean == 1) {
     pthread_mutex_lock(&mutexBanco);
     printf("\n\n\t----- Banco [Antes de pagar a parcela] -----\n->Especie: R$ %.2f\n->Taxas: R$ %.2f\n->Subsidio: R$ %.2f\n", instFinanceira.especie, instFinanceira.taxas, instFinanceira.subsidio);
     pthread_mutex_unlock(&mutexBanco);
@@ -190,7 +197,7 @@ void devolverDinheiro(financiamento *pessoa) {
   instFinanceira.taxas += pessoa->taxas;
   instFinanceira.subsidio += pessoa->subsidio;
 
-  printf("Pessoa %d devolveu o dinheiro com sucesso!\n", pessoa->id);
+  printf("\nPessoa %d devolveu o dinheiro com sucesso!", pessoa->id);
 
   pthread_mutex_unlock(&mutexBanco);
 }
@@ -218,18 +225,19 @@ void emprestimoBanco(financiamento *pessoa) {
     }
 
     pessoa->financiado = 1;
+    pessoa->momentoFinanciamento = countWhile;
 
     printf("\nValor Total do Imovel: %.2f\nPessoa %d precisa de R$%.2f de especie e R$ %.2f de taxa", pessoa->totalImovel, pessoa->id, valorEmprestimoEspecie, valorEmprestimoTaxa);
     pthread_mutex_unlock(&mutexBanco);
   } else {
-    printf("Nao foi possivel liberar o emprestimo para a pessoa %d", pessoa->id);
+    printf("Nao foi possivel liberar o emprestimo para a pessoa %d\n", pessoa->id);
     pthread_mutex_unlock(&mutexBanco);
   }
 }
 
 void verificaPessoasAtendidas() {
   if (pessoasAtendidas >= 3) {
-    printf("Banco atendeu 3 pessoas e recebeu um bônus de 50%% de subsidio");
+    printf("Banco atendeu 3 pessoas e recebeu um bônus de 50%% de subsidio!\n");
     instFinanceira.subsidio += instFinanceira.subsidio * 0.5;
     pessoasAtendidas = 0;
   }
